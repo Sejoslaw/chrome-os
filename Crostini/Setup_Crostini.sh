853797e9662756a618901ba90a80a10376bdbf73
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2025-03-09 12:57:23 +0000

    +configure jetbrains;

--- Crostini/Setup_Crostini.sh
+++ Crostini/Setup_Crostini.sh
@@ -137,7 +137,17 @@ then
     apt install wget gpg
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
     rm -f packages.microsoft.gpg
     apt install apt-transport-https && apt update && apt install code
+fi
+
+echo ""
+read -p "## Configure JetBrains (IntelliJ, PyCharm, Rider, etc.) unofficial PPA? (yes/empty)" _configureJetBrains;
+
+if [ "$_configureJetBrains" != "" ]
+then
+    curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | gpg --dearmor | sudo tee /usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg > /dev/null
+    echo "deb [signed-by=/usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg] http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com any main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
+    sudo apt update
 fi
