d15b69c8c1c9f23b775d2f963bd30c758f5ebd0a
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2025-03-09 12:51:36 +0000

    +vs code;

--- Crostini/Setup_Crostini.sh
+++ Crostini/Setup_Crostini.sh
@@ -128,3 +128,16 @@ if [ "$_installATLauncher" != "" ]
 then
     install_file_from_url "https://download.nodecdn.net/containers/atl/app/dist/linux/" "atlauncher-1.4-1.deb"
 fi
+
+echo ""
+read -p "## Install VS Code (from Microsoft repository)? (yes/empty)" _installCode;
+
+if [ "$_installCode" != "" ]
+then
+    apt install wget gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    rm -f packages.microsoft.gpg
+    apt install apt-transport-https && apt update && apt install code
+fi
