32de02ffbc3a5a979f597e1969cb9b70f748fbf0
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2025-05-06 19:55:13 +0000

    +kubectl 1.33;

--- Crostini/Setup_Crostini.sh
+++ Crostini/Setup_Crostini.sh
@@ -170,3 +170,17 @@ then
     sudo apt update
     sudo apt install rancher-desktop
 fi
+
+echo ""
+read -p "## Install kubectl (official website)? (yes/empty)" _installKubectl;
+
+if [ "$_installKubectl" != "" ]
+then
+    sudo apt install -y apt-transport-https ca-certificates curl gnupg
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
+    sudo apt update
+    sudo apt install kubectl -y
+fi
