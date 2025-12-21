ca5990c7727922d37195ad4083a6f05b8d0b9ef3
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2025-12-21 20:54:25 +0000

    !setup podman containers;

--- Crostini/Setup_Crostini_Podman_Containers.sh
+++ Crostini/Setup_Crostini_Podman_Containers.sh
@@ -19,86 +19,52 @@ then
     podman build -t fedora-toolbox -f ./Podman/Containerfiles/Terminal/fedora-toolbox.containerfile .
 
     # Build GUI-configured images
-    sh ./Podman/Containerfiles/Fedora-Toolbox-GUI/fedora-toolbox-gui.build.sh
+    podman build -t fedora-toolbox-gui -f ./Podman/Containerfiles/GUI/Fedora-Toolbox-GUI/fedora-toolbox-gui.containerfile .
     
     # Build application-configured images
-    sh ./Podman/Containerfiles/Brave-Disposable-Hardened/fedora-toolbox-gui-brave.build.sh
-    sh ./Podman/Containerfiles/Fedora-Toolbox-GUI-xfce/fedora-toolbox-gui-xfce.build.sh
+    podman build -t fedora-toolbox-gui-brave -f ./Podman/Containerfiles/GUI/Brave-Disposable-Hardened/fedora-toolbox-gui-brave.containerfile .
+    podman build -t fedora-toolbox-gui-xfce -f ./Podman/Containerfiles/GUI/Fedora-Toolbox-GUI-xfce/fedora-toolbox-gui-xfce.containerfile .
+
+    # Copy runners
+    mkdir podman-runner
+    cp ./Podman/Containerfiles/Fedora-Toolbox-GUI/fedora-toolbox-gui.run.sh ~/podman-runner/fedora-toolbox-gui.run.sh
+    cp ./Podman/Containerfiles/Fedora-Toolbox-GUI-xfce/fedora-toolbox-gui-xfce.run.sh ~/podman-runner/fedora-toolbox-gui-xfce.run.sh
+
+    cp ./Podman/Containerfiles/Brave-Disposable-Hardened/fedora-toolbox-gui-brave.run.sh ~/podman-runner/fedora-toolbox-gui-brave.run.sh
+    cp ./Podman/Containerfiles/Brave-Disposable-Hardened/fedora-toolbox-gui-brave-persistent.run.sh ~/podman-runner/fedora-toolbox-gui-brave-persistent.run.sh
     
-    cat <<EOF >> ~/.test
+    # Prepare .bashrc file
+    cat <<EOF >> ~/.bashrc
 
     alias update="sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y"
 
-    fedora-disp() {
-        podman run -it --rm \\
-            --userns keep-id \\
-            --ipc=host \\
-            --cap-drop=all \\
-            --cap-add={net_raw,audit_write,chown,setuid,setgid,dac_override} \\
-            --security-opt label=disable \\
-            --security-opt seccomp=unconfined \\
-            --device /dev/dri/card0:/dev/dri/card0 \\
-            --device /dev/dri/renderD128:/dev/dri/renderD128 \\
-            -v /tmp/.X11-unix:/tmp/.X11-unix:ro \\
-            -v $XDG_RUNTIME_DIR/wayland-0:$XDG_RUNTIME_DIR/wayland-0:ro \\
-            -v $XDG_RUNTIME_DIR/pulse:/run/user/1000/pulse:ro \\
-            --tmpfs /run/user/1000:mode=700,size=512M \\
-            --tmpfs /home/fedorauser:mode=755,size=10G \\
-            fedora-toolbox-gui-xfce:latest
+    disp-arch-toolbox() {
+        podman run --rm -it arch-toolbox:latest
     }
 
-    brave-disp() {
-        podman run -it --rm \\
-            --read-only \\
-            --memory=4g \\
-            --cpus=2 \\
-            --userns keep-id \\
-            --ipc=host \\
-            --cap-drop=all \\
-            --cap-add={net_raw,chown,setuid,setgid} \\
-            --security-opt label=disable \\
-            --security-opt seccomp=unconfined \\
-            --security-opt no-new-privileges \\
-            --device /dev/dri/card0:/dev/dri/card0 \\
-            --device /dev/dri/renderD128:/dev/dri/renderD128 \\
-            -v /tmp/.X11-unix:/tmp/.X11-unix:ro \\
-            -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/run/user/1000/wayland-0:ro \\
-            -v $XDG_RUNTIME_DIR/pulse:/run/user/1000/pulse:ro \\
-            -v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket:ro \\
-            --tmpfs /run/user/1000:mode=700,size=512M \\
-            --tmpfs /home/fedorauser:mode=755,size=4G \\
-            --tmpfs /tmp:mode=1777,size=1G \\
-            --tmpfs /var/tmp:mode=1777,size=512M \\
-            fedora-toolbox-gui-brave:latest
+    disp-debian-toolbox() {
+        podman run --rm -it debian-toolbox:latest
     }
 
-    brave-persistent() {
-        VOLUME_NAME="brave-persistent-volume"
-
-        podman volume exists "$VOLUME_NAME" || podman volume create "$VOLUME_NAME"
-
-        podman run -it --rm \\
-            --read-only \\
-            --memory=4g \\
-            --cpus=2 \\
-            --userns keep-id \\
-            --ipc=host \\
-            --cap-drop=all \\
-            --cap-add={net_raw,chown,setuid,setgid} \\
-            --security-opt label=disable \\
-            --security-opt seccomp=unconfined \\
-            --security-opt no-new-privileges \\
-            --device /dev/dri/card0:/dev/dri/card0 \\
-            --device /dev/dri/renderD128:/dev/dri/renderD128 \\
-            -v /tmp/.X11-unix:/tmp/.X11-unix:ro \\
-            -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/run/user/1000/wayland-0:ro \\
-            -v $XDG_RUNTIME_DIR/pulse:/run/user/1000/pulse:ro \\
-            -v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket:ro \\
-            -v "$VOLUME_NAME":/home/fedorauser:Z \\
-            --tmpfs /run/user/1000:mode=700,size=512M \\
-            --tmpfs /tmp:mode=1777,size=1G \\
-            --tmpfs /var/tmp:mode=1777,size=512M \\
-            fedora-toolbox-gui-brave:latest
+    disp-fedora-toolbox() {
+        podman run --rm -it fedora-toolbox:latest
     }
+
+    disp-fedora-gui() {
+        sh ~/podman-runner/fedora-toolbox-gui.run.sh
+    }
+
+    disp-fedora-gui-xfce() {
+        sh ~/podman-runner/fedora-toolbox-gui-xfce.run.sh
+    }
+
+    disp-fedora-gui-brave() {
+        sh ~/podman-runner/fedora-toolbox-gui-brave.run.sh
+    }
+
+    disp-fedora-gui-brave-persistent-volume() {
+        sh ~/podman-runner/fedora-toolbox-gui-brave-persistent.run.sh
+    }
+
     EOF
 fi
