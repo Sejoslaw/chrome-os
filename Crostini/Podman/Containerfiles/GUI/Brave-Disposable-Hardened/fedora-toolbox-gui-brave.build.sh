e67629a032382a441d654d5b05688029c00d8923
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2025-12-21 14:00:56 +0000

    !cleaning - per directory split; +hardened brave container;

---
+++ Crostini/Podman/Containerfiles/GUI/Brave-Disposable-Hardened/fedora-toolbox-gui-brave.build.sh
@@ -0,0 +1,1 @@
+podman build -t fedora-toolbox-gui-brave -f fedora-toolbox-gui-brave.containerfile .
---
+++ Crostini/Podman/Containerfiles/GUI/Brave-Disposable-Hardened/fedora-toolbox-gui-brave.run.sh
@@ -0,0 +1,22 @@
+podman run -it --rm \
+  --read-only \
+  --memory=4g \
+  --cpus=2 \
+  --userns keep-id \
+  --ipc=host \
+  --cap-drop=all \
+  --cap-add={net_raw,chown,setuid,setgid} \
+  --security-opt label=disable \
+  --security-opt seccomp=unconfined \
+  --security-opt no-new-privileges \
+  --device /dev/dri/card0:/dev/dri/card0 \
+  --device /dev/dri/renderD128:/dev/dri/renderD128 \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/run/user/1000/wayland-0:ro \
+  -v $XDG_RUNTIME_DIR/pulse:/run/user/1000/pulse:ro \
+  -v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket:ro \
+  --tmpfs /run/user/1000:mode=700,size=512M \
+  --tmpfs /home/fedorauser:mode=755,size=4G \
+  --tmpfs /tmp:mode=1777,size=1G \
+  --tmpfs /var/tmp:mode=1777,size=512M \
+  fedora-toolbox-gui-brave:latest
