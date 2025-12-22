13533cc00df70324f3e5fee7b1a1a056544c54c5
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2025-12-22 22:08:42 +0000

    +personal setup script;

---
+++ Crostini/Setup_Crostini_krisu.sh
@@ -0,0 +1,48 @@
+echo ""
+echo "################################################################"
+echo "##                                                            ##"
+echo "##                    Updating Crostini...                    ##"
+echo "##                                                            ##"
+echo "################################################################"
+echo ""
+
+mv /usr/share/applications/vim.desktop /usr/share/applications/vim.desktop.old
+apt update && apt full-upgrade -y && apt autoremove -y
+
+echo "" >> /home/$0/.bashrc
+echo "alias update-crostini='sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && flatpak update'" >> /home/$0/.bashrc
+
+echo ""
+echo "################################################################"
+echo "##                                                            ##"
+echo "##                   Setting up utilities...                  ##"
+echo "##                                                            ##"
+echo "################################################################"
+echo ""
+
+apt install podman -y
+
+if ! grep -q "$0" /etc/subuid; then
+    echo "$0:100000:65536" | sudo tee -a /etc/subuid
+    echo "$0:100000:65536" | sudo tee -a /etc/subgid
+fi
+
+echo ""
+echo "################################################################"
+echo "##                                                            ##"
+echo "##                    Setting up Flatpaks...                  ##"
+echo "##                                                            ##"
+echo "################################################################"
+echo ""
+
+apt install flatpak -y
+
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install -y flathub com.github.tchx84.Flatseal
+flatpak install -y flathub io.podman_desktop.PodmanDesktop
+flatpak install -y flathub com.brave.Browser
+flatpak install -y flathub com.valvesoftware.Steam
+flatpak install -y flathub com.atlauncher.ATLauncher
+flatpak install -y flathub org.gnome.Boxes
+flatpak install -y flathub com.usebottles.bottles
--- README.md
+++ README.md
@@ -6,3 +6,11 @@ cd /tmp
 git clone https://github.com/Sejoslaw/chrome-os
 sh ./chrome-os/Crostini/Setup_Crostini.sh
 ```
+
+Or you can use my config directly
+
+```bash
+cd /tmp
+git clone https://github.com/Sejoslaw/chrome-os
+sudo sh ./chrome-os/Crostini/Setup_Crostini_krisu.sh $USER
+```
