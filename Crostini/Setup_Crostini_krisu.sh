07e26a392b156fc4e1fdae1af870a07c60955893
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2026-01-05 18:11:40 +0100

    Update script to install Flatpak and related apps

--- Crostini/Setup_Crostini_krisu.sh
+++ Crostini/Setup_Crostini_krisu.sh
@@ -10,19 +10,20 @@ mv /usr/share/applications/vim.desktop /usr/share/applications/vim.desktop.old
 apt update && apt full-upgrade -y && apt autoremove -y
 
 echo "" >> /home/$1/.bashrc
-echo "sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y" >> /home/$1/.bashrc
+echo "sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo flatpak update" >> /home/$1/.bashrc
 
 echo ""
 echo "################################################################"
 echo "##                                                            ##"
-echo "##                    Setting up Podman...                    ##"
+echo "##                    Setting up Flatpaks...                  ##"
 echo "##                                                            ##"
 echo "################################################################"
 echo ""
 
-apt install podman -y
+apt install flatpak -y
 
-if ! grep -q "$1" /etc/subuid; then
-    echo "$1:100000:65536" | sudo tee -a /etc/subuid
-    echo "$1:100000:65536" | sudo tee -a /etc/subgid
-fi
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install -y flathub com.github.tchx84.Flatseal
+flatpak install -y flathub com.brave.Browser
+flatpak install -y flathub org.gnome.Boxes # /var/lib/flatpak/exports/share/applications/org.gnome.Boxes.desktop
