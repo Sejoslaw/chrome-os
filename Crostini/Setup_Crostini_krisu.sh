6bbfc85f172261a6753f2b4fb21e42700d0b48a9
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2026-01-16 18:18:14 +0000

    !fix;

--- Crostini/Setup_Crostini_krisu.sh
+++ Crostini/Setup_Crostini_krisu.sh
@@ -28,7 +28,6 @@ echo "##                                                            ##"
 echo "################################################################"
 echo ""
 
-apt install curl -y
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
 apt update && apt install brave-browser -y
