ad03c7f6dd71d38480f67ed06f57a35d9cbffe49
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2026-02-13 09:03:57 +0100

    Install GNOME Boxes using Flatpak
    Added installation command for GNOME Boxes via Flatpak.

--- Crostini/Setup_Crostini_krisu.sh
+++ Crostini/Setup_Crostini_krisu.sh
@@ -33,3 +33,4 @@ echo ""
 
 flatpak install -y flathub com.github.tchx84.Flatseal
 flatpak install -y flathub com.brave.Browser
+flatpak install -y flathub org.gnome.Boxes # /var/lib/flatpak/exports/share/applications/org.gnome.Boxes.desktop <-- "sommelier -X --scale=1.0 --dpi=96"
