b5c17d40194175009ba3558815f09a729d489851
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2026-04-16 09:38:33 +0200

    Add additional Flatpak applications to installation script

--- Crostini/Setup_Crostini_krisu.sh
+++ Crostini/Setup_Crostini_krisu.sh
@@ -31,4 +31,8 @@ echo "##                                                            ##"
 echo "################################################################"
 echo ""
 
-sudo flatpak install -y flathub com.github.tchx84.Flatseal com.brave.Browser
+sudo flatpak install -y flathub \
+  com.github.tchx84.Flatseal \
+  com.brave.Browser \
+  org.virt_manager.virt-manager \
+  org.virt_manager.virt_manager.Extension.Qemu
