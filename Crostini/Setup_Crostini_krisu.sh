71756626f8b3261cdaa740eb99a00ba29e4bacba
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2026-06-26 19:33:54 +0200

    Add command to append to .bashrc in setup script

--- Crostini/Setup_Crostini_krisu.sh
+++ Crostini/Setup_Crostini_krisu.sh
@@ -23,6 +23,7 @@ echo ""
 sudo apt install flatpak -y
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+echo "" >> /home/$1/.bashrc
 echo "sudo flatpak update" >> /home/$1/.bashrc
 
 sudo flatpak install -y flathub \
