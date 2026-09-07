2e875908617e6a30d285a33df24632e6e2e8a089
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2026-09-07 17:55:02 +0200

    Add -y flag to Chrome installation command
    Added '-y' flag to apt install command for automatic confirmation.

--- Setup_Crostini.sh
+++ Setup_Crostini.sh
@@ -21,7 +21,7 @@ echo "################################################################"
 echo ""
 
 curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
 rm google-chrome-stable_current_amd64.deb
 
 echo ""
